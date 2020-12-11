51c254e417c6e5f7d1299ff57f1ea43e77a95fba
mahmoud reda <55753782+MAHMOUDRR707@users.noreply.github.com>
2020-12-11 15:52:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -83,12 +83,6 @@ def predict():
 
     return None
 
-
 if __name__ == '__main__':
-    # app.run(port=5002, threaded=False)
-
-    # Serve the app with gevent
-    http_server = WSGIServer(('0.0.0.0', 5000), app)
-    http_server.serve_forever()
-
-
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True)
